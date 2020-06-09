2e4b341f4fe5c2e9a4ed931ecd2a2755b965a586
KOFITT <erentaskin.34@icloud.com>
2020-06-10 00:36:28 +0300

    Create dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,45 @@
+students = {}
+
+number = input('Number: ')
+name = input('Name: ')
+surname = input('Surname: ')
+telephone = input('Telephone :')
+
+students.update({
+    number: {
+        'Name' : name ,
+        'Surname' : surname ,
+        'Telephone' : telephone 
+    }
+})
+
+number = input('Number: ')
+name = input('Name: ')
+surname = input('Surname: ')
+telephone = input('Telephone :')
+
+students.update({
+    number: {
+        'Name' : name ,
+        'Surname' : surname ,
+        'Telephone' : telephone 
+    }
+})
+
+
+number = input('Number: ')
+name = input('Name: ')
+surname = input('Surname: ')
+telephone = input('Telephone :')
+
+students.update({
+    number: {
+        'Name' : name ,
+        'Surname' : surname ,
+        'Telephone' : telephone 
+    }
+})
+
+stdnumber = input('Which number you want to check? :')
+
+print(f"Number you check is {stdnumber}, name of the student {students[stdnumber]['Name']}, surname of the student {students[stdnumber]['Surname']}, telephone number of the student {students[stdnumber]['Telephone']}.")
