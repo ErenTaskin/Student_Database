ce97683830afc7e5c6384e571971a0f47705f4eb
KOFIT <erentaskin.34@icloud.com>
2020-06-10 00:43:23 +0300

    Update Student Database.py

--- Student Database.py
+++ Student Database.py
@@ -40,6 +40,8 @@ students.update({
     }
 })
 
+print('*' * 100)
+
 stdnumber = input('Which number you want to check? :')
 
 print(f"Number you check is {stdnumber}, name of the student {students[stdnumber]['Name']}, surname of the student {students[stdnumber]['Surname']}, telephone number of the student {students[stdnumber]['Telephone']}.")
